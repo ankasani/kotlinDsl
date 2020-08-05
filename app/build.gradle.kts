50547a8f8003e9e2162f48694f91acfb79913bd1
ankasani <ankasani@microsoft.com>
2020-08-05 13:25:04 +0530

    added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,91 +1,37 @@
 apply plugin: 'com.android.application'
 plugins {
-    id(BuildPlugins.androidApplication)
-    id(BuildPlugins.kotlinAndroidPlugin)
-    id(BuildPlugins.kotlinAndroidExtensionsPlugin)
-    id(BuildPlugins.kotlinAndroidKaptPlugin)
-    id(BuildPlugins.googleServicesPlugin)
-    id(BuildPlugins.crashlyticsPlugin)
-    id(BuildPlugins.safeArgsPlugin)
+    id(Plugins.androidApplication)
+    id(Plugins.kotlinAndroid)
+    id(Plugins.kotlinAndroidExtensions)
 }
 
 android {
-    compileSdkVersion(AndroidSdk.compileApi)
-    buildToolsVersion(AndroidSdk.buildTools)
+    compileSdkVersion(Versions.compileSdkVersion)
     defaultConfig {
-        minSdkVersion(AndroidSdk.minApi)
-        targetSdkVersion(AndroidSdk.targetApi)
-        versionCode = 1
-        versionName = "1.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        javaCompileOptions {
-            annotationProcessorOptions {
-                arguments.apply {
-                    put("room.schemaLocation", "$projectDir/schemas")
-                    put("room.incremental", "true")
-                    put("room.expandProjection", "true")
-                }
-            }
-        }
+        applicationId = Config.applicatiınId
+        minSdkVersion(Versions.minSdkVersion)
+        targetSdkVersion(Versions.targetSdkVersion)
+        versionCode = Release.versionCode
+        versionName = Release.versionName
+        testInstrumentationRunner = Config.testInstrumentationRunner
     }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
-    }
-
-    flavorDimensions("default")
-    productFlavors {
-        create("dev") {
-            versionNameSuffix = " DEV"
-        }
-        create("prod")
-        create("sim") {
-            versionNameSuffix = " DEV-SIMULATED"
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    testOptions {
-        unitTests.isReturnDefaultValues = true
     }
 }
 
 dependencies {
-    implementation(Libraries.kotlinStdLib)
-    implementation(Libraries.coroutines)
-    implementation(Libraries.material)
-    implementation(Libraries.appCompat)
-    implementation(Libraries.ktxCore)
-    implementation(Libraries.constraintLayout)
-    koin()
-    lifecycle()
-    navigation()
-    network()
-    firebase()
-    room()
-    implementation(Libraries.swiperefreshlayout)
-    implementation(Libraries.coroutinesPlayServices)
+    // Core Libraries
+    implementation(CoreLibraries.kotlin)
 
-    testImplementation(TestLibraries.junit4)
-    testImplementation(TestLibraries.coroutinesTesting)
-    testImplementation(TestLibraries.testing)
-    testImplementation(TestLibraries.mockitoKotlin)
-    androidTestImplementation(TestLibraries.testRunner)
-    androidTestImplementation(TestLibraries.espresso)
-    androidTestImplementation(TestLibraries.testing)
-    androidTestImplementation(TestLibraries.junit4)
-    androidTestImplementation(TestLibraries.mockitoKotlin)
-    androidTestImplementation(TestLibraries.coroutinesTesting)
+    // Support Libraries
+    implementation(SupportLibraries.appCompat)
+
+    // Testing
+    testImplementation(TestLibraries.jUnit)
+    androidTestImplementation(TestLibraries.runnner)
+    androidTestImplementation(TestLibraries.espressoCore)
 }
