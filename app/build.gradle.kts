a11d214a9f6fb8d3539ee032a9ff44328ccc469b
sum0999 <sum@microsoft.com>
2021-02-12 16:57:15 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id(BuildPlugins.androidApplication)
+    //id(BuildPlugins.androidApplication)
     kotlin(BuildPlugins.android)
     id(BuildPlugins.kotlinAndroidExtensions)
     id(BuildPlugins.kotlinKapt)
