b5263b40485e0402c34e9c4b1c96b433d05d0fd6
ankasani <ankasani@microsoft.com>
2021-05-28 12:37:45 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,85 @@
-//apply plugin: 'com.android.application'
 plugins {
-    id("com.android.application")
-    id(Plugins.kotlinAndroid)
-    id(Plugins.kotlinAndroidExtensions)
+    id(BuildPlugins.androidApplication)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExtensions)
+    id(BuildPlugins.kotlinAnnotationProcessor)
+    id(BuildPlugins.androidxNavSafeArgs)
+    id(BuildPlugins.pluginGoogleService)
+    id(BuildPlugins.plugin_crashlytics)
 }
 
 android {
-    compileSdkVersion(Versions.compileSdkVersion)
+    compileSdkVersion(Versions.compileSdk)
     defaultConfig {
-        applicationId = Config.applicationId
-        minSdkVersion(Versions.minSdkVersion)
-        targetSdkVersion(Versions.targetSdkVersion)
-        versionCode = Release.versionCode
-        versionName = Release.versionName
-        //versionName = "@ # $ % ^ * - +"
-        testInstrumentationRunner = Config.testInstrumentationRunner
+        applicationId = ApplicationId.id
+        minSdkVersion(Versions.minSdk)
+        targetSdkVersion(Versions.targetSdk)
+        versionCode = Releases.versionCode
+        versionName = Releases.versionName
+        multiDexEnabled = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
     buildTypes {
-        getByName ("release") {
+        getByName("release") {
+            resValue("string", "app_name", "kitchen-display-service-android")
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
+            extra.set("enableCrashlytics", true)
+            manifestPlaceholders = mapOf("crashlyticsCollectionEnabled" to "true")
         }
-    }  
-    flavorDimensions("default")
+
+        getByName("debug") {
+            buildConfigField("boolean", "MYVALUE", "true")
+            resValue("string", "app_name", "KDS Debug")
+            applicationIdSuffix = ".debug"
+
+            extra.set("enableCrashlytics", false)
+            manifestPlaceholders = mapOf("crashlyticsCollectionEnabled" to "false")
+        }
+    }
+
+    flavorDimensions("odoo")
     productFlavors {
-        create("dev") {
-            versionNameSuffix = " DEV"
+        create(Flavor.SIT) {
+            setDimension("odoo")
+            buildConfigField("String", "ODOO_URL", Environment.ODOO_SIT)
         }
-        create("prod")
-        create("sim") {
-            versionNameSuffix = " DEV-SIMULATED"
+
+        create(Flavor.STAGING) {
+            setDimension("odoo")
+            buildConfigField("String", "ODOO_URL", Environment.ODOO_STAGING)
         }
+
+        create(Flavor.PROD) {
+            setDimension("odoo")
+            buildConfigField("String", "ODOO_URL", Environment.ODOO_PROD)
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
     }
 }
 
 dependencies {
-    // Core Libraries
-    implementation(CoreLibraries.kotlin)
+    implementation(project(Modules.core))
+    implementation(project(Modules.datasource))
+    implementation(project(Modules.featureMain))
 
-    // Support Libraries
-    implementation(SupportLibraries.appCompat)
-
-    // Testing
-    testImplementation(TestLibraries.jUnit)
-    androidTestImplementation(TestLibraries.runnner)
-    androidTestImplementation(TestLibraries.espressoCore)
+    // Firebase
+    api(Dependencies.firebaseAnalytics)
+    api(Dependencies.firebaseMessaging)
+    api(Dependencies.firebaseCrashlytics)
+    implementation(project(Modules.featureHome))
+    implementation(project(Modules.featureLogin))
 }
